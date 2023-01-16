8c33828d3b61a1ecdf0581d832aaeac6c1c51b2a
IvanKhudyakov <halex7@yandex.ru>
2023-01-16 19:47:46 +0300

    8. GettingStarted with LoginUI

---
+++ TwitterTutorial/Controller/Autentication/RegistrationController.swift
@@ -0,0 +1,27 @@
+//
+//  RegistrationController.swift
+//  TwitterTutorial
+//
+//  Created by Иван Худяков on 16.01.2023.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+    //MARK: -Properties
+    
+    //MARK: -Lyfecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    //MARK: -Selectors
+    
+    //MARK: -Helpers
+    
+    func configureUI() {
+        view.backgroundColor = .twitterBlue
+    }
+}
+
