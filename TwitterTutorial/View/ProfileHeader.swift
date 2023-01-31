5c7f5be5794169e9b02583a4da46bdb74850848a
IvanKhudyakov <halex7@yandex.ru>
2023-01-31 13:33:20 +0300

    39. Profile CollectionView Setup

---
+++ TwitterTutorial/View/ProfileHeader.swift
@@ -0,0 +1,32 @@
+//
+//  ProfileHeader.swift
+//  TwitterTutorial
+//
+//  Created by Иван Худяков on 30.01.2023.
+//
+
+import UIKit
+
+class ProfileHeader: UICollectionReusableView {
+    //MARK: -Properties
+
+    
+    //MARK: -Lyfecycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .red
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+
+    //MARK: -Selectors
+    
+   
+    //MARK: -Helpers
+    
+    
+}
