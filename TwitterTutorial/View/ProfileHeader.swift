94f7a76451b6325fce0824906cc80d7d7bf04285
IvanKhudyakov <halex7@yandex.ru>
2023-01-31 15:15:26 +0300

    40. Setup Profile Header UICollectionReusableView

--- TwitterTutorial/View/ProfileHeader.swift
+++ TwitterTutorial/View/ProfileHeader.swift
@@ -9,13 +9,117 @@ import UIKit
 
 class ProfileHeader: UICollectionReusableView {
     //MARK: -Properties
+    
+    private lazy var containerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .twitterBlue
+        
+        view.addSubview(backButton)
+        backButton.anchor(top: view.topAnchor,
+                          left: view.leftAnchor,
+                          paddingTop: 42,
+                          paddingLeft: 16)
+        backButton.setDimensions(width: 30, height: 30)
+        
+        return view
+    }()
 
+    private lazy var backButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "baseline_arrow_back_white_24dp")?.withRenderingMode(.alwaysOriginal), for: .normal)
+        button.addTarget(self, action: #selector(handleDismissal), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    private let profileImageView: UIImageView = {
+       let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.layer.cornerRadius = 80 / 2
+        iv.clipsToBounds = true
+        iv.backgroundColor = .lightGray
+        iv.layer.borderColor = UIColor.white.cgColor
+        iv.layer.borderWidth = 4
+        
+        return iv
+    }()
+    
+    private lazy var editProfileFollowButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Loading", for: .normal)
+        button.titleLabel?.textAlignment = .center
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.twitterBlue, for: .normal)
+        button.layer.cornerRadius = 32 / 2
+        button.layer.borderColor = UIColor.twitterBlue.cgColor
+        button.layer.borderWidth = 1.25
+        
+        button.addTarget(self, action: #selector(handleEditProfileFollow), for: .touchUpInside)
+        return button
+    }()
+    
+    private let bioLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.numberOfLines = 3
+        label.text = "This is a user bio will span more than one line for test purpose"
+        
+        return label
+    }()
+    
+    let fullNameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        label.text = "Eddie Brock"
+        
+        return label
+    }()
+    
+    let userNameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.textColor = .lightGray
+        label.text = "@venom"
+        
+        return label
+    }()
     
     //MARK: -Lyfecycle
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        backgroundColor = .red
+        addSubview(containerView)
+        containerView.anchor(top: topAnchor,
+                             left: leftAnchor,
+                             right: rightAnchor,
+                             height: 108)
+        
+        addSubview(profileImageView)
+        profileImageView.anchor(top: containerView.bottomAnchor,
+                                left: leftAnchor,
+                                paddingTop: -24,
+                                paddingLeft: 8)
+        profileImageView.setDimensions(width: 80, height: 80)
+        
+        addSubview(editProfileFollowButton)
+        editProfileFollowButton.anchor(top: containerView.bottomAnchor,
+                                       right: rightAnchor,
+                                       paddingTop: 12,
+                                       paddingRight: 16)
+        editProfileFollowButton.setDimensions(width: 100, height: 36)
+        
+        let userDetailStack = UIStackView(arrangedSubviews: [fullNameLabel, userNameLabel, bioLabel])
+        userDetailStack.axis = .vertical
+        userDetailStack.distribution = .fillProportionally
+        userDetailStack.spacing = 4
+        
+        addSubview(userDetailStack)
+        userDetailStack.anchor(top: profileImageView.bottomAnchor,
+                               left: leftAnchor,
+                               right: rightAnchor,
+                               paddingTop: 8,
+                               paddingLeft: 12,
+                               paddingRight: 12)
     }
     
     required init?(coder: NSCoder) {
@@ -24,6 +128,13 @@ class ProfileHeader: UICollectionReusableView {
     
 
     //MARK: -Selectors
+    @objc func handleDismissal() {
+        
+    }
+    
+    @objc func handleEditProfileFollow() {
+        
+    }
     
    
     //MARK: -Helpers
